444c71ceda605f898015b36a862cf961d522673c
millidavids <david.a.yurek@gmail.com>
2017-08-02 18:14:40 -0400

    working proxy with react scripts, may consider express as production proxy instead of nginx

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import { Route, Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import './App.css'
 
 class App extends Component {
+  static propTypes = {
+    apikey: PropTypes.string.isRequired
+  }
+
+  state = {
+    data: {}
+  }
+
+  componentWillMount = () => {
+    let headers = new Headers()
+    headers.append("TRN-API-KEY", this.props.apikey)
+    fetch(`/api/profile/pc/millidavids`, {headers: headers})
+      .then(res => res.json())
+      .then(data => this.setState({data: data}))
+  }
+
   render() {
+    console.log(this.state.data)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
